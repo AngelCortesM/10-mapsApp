b527e6fdac972a00af646c67141ccd49e57cb0b5
{AngelCortesM} <joseangelcortesmontoya@gmail.com>
2024-12-26 17:24:22 -0500

    Agregar integración de Mapbox y componentes de mapa a pantalla completa y rango de zoom

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Map } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
 @Component({
   standalone: false,
-  
-  templateUrl: './zoom-range-page.component.html',
-  styleUrl: './zoom-range-page.component.css'
-})
-export class ZoomRangePageComponent {
 
+  templateUrl: './zoom-range-page.component.html',
+  styleUrl: './zoom-range-page.component.css',
+})
+export class ZoomRangePageComponent implements AfterViewInit {
+  @ViewChild('map') divMap?: ElementRef;
+
+  public zoom: number = 10;
+  public map?: Map;
+
+  ngAfterViewInit(): void {
+    if (!this.divMap) throw new Error('No divMap reference');
+    this.map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: this.zoom, // starting zoom
+    });
+    this.mapListeners();
+  }
+
+  mapListeners() {
+    if (!this.map) throw new Error('No map reference');
+    this.map.on('zoom', (ev) => {
+      this.zoom = this.map!.getZoom();
+    });
+
+    this.map.on('zoomend', (ev) => {
+      if (this.map!.getZoom() < 18) return;
+      this.map!.zoomTo(18);
+    });
+  }
+  zoomIn() {
+    this.map?.zoomIn();
+  }
+  zoomOut() {
+    this.map?.zoomOut();
+  }
+  zoomChanged(value: string) {
+    this.zoom = Number(value);
+    this.map?.zoomTo(this.zoom);
+  }
 }
