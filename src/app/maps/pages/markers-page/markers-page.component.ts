f63f8a1a9c89a49523c4a35ecca4745ddfeec8e0
AngelCortesM <joseangelcortesmontoya@gmail.com>
2024-12-27 21:54:15 -0500

    Agregar funcionalidad de marcadores y ajustes de zoom en la página de mapas

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Map, LngLat, Marker } from 'mapbox-gl';
 
 @Component({
   standalone: false,
-  
-  templateUrl: './markers-page.component.html',
-  styleUrl: './markers-page.component.css'
-})
-export class MarkersPageComponent {
 
+  templateUrl: './markers-page.component.html',
+  styleUrl: './markers-page.component.css',
+})
+export class MarkersPageComponent implements AfterViewInit {
+  @ViewChild('map') divMap?: ElementRef;
+
+  public zoom: number = 10;
+  public map?: Map;
+  public currentLngLat: LngLat = new LngLat(-74.1, 4.65);
+
+  ngAfterViewInit(): void {
+    if (!this.divMap) throw new Error('No divMap reference');
+
+    this.map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.currentLngLat, // starting position [lng, lat]
+      zoom: this.zoom, // starting zoom
+    });
+
+    // const markerHtml = document.createElement('div');
+    // markerHtml.innerHTML = 'Fernando herrera';
+
+    // const marker = new Marker({
+    //   color: '#224422',
+    //   element: markerHtml,
+    // })
+    //   .setLngLat(this.currentLngLat)
+    //   .addTo(this.map);
+  }
+
+  createMarker() {
+    if (!this.map) return;
+    const generateBeautifulColor = () => {
+      const hue = Math.floor(Math.random() * 360); // Tono: 0-360
+      const saturation = Math.random() * 30 + 70; // Saturación: 70-100%
+      const lightness = Math.random() * 30 + 50; // Luminosidad: 50-80%
+      const alpha = (Math.random() * 0.4 + 0.6).toFixed(2); // Transparencia: 0.6-1.0
+      return `hsla(${hue}, ${saturation}%, ${lightness}%, ${alpha})`;
+    };
+    const color = generateBeautifulColor();
+    const lgnLat = this.map.getCenter();
+    this.addMarker(lgnLat, color);
+  }
+  addMarker(lngLat: LngLat, color: string) {
+    if (!this.map) return;
+    const marker = new Marker({
+      color: color,
+      draggable: true,
+    })
+      .setLngLat(lngLat)
+      .addTo(this.map!);
+  }
 }
--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,5 +1,11 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { Map } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  ViewChild,
+} from '@angular/core';
+import { LngLat, Map } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
 @Component({
   standalone: false,
@@ -7,23 +13,26 @@ import { Map } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
   templateUrl: './zoom-range-page.component.html',
   styleUrl: './zoom-range-page.component.css',
 })
-export class ZoomRangePageComponent implements AfterViewInit {
+export class ZoomRangePageComponent implements AfterViewInit, OnDestroy {
   @ViewChild('map') divMap?: ElementRef;
 
   public zoom: number = 10;
   public map?: Map;
+  public currentlngLat: LngLat = new LngLat(-74.1, 4.65);
 
   ngAfterViewInit(): void {
     if (!this.divMap) throw new Error('No divMap reference');
     this.map = new Map({
       container: this.divMap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
+      center: this.currentlngLat, // starting position [lng, lat]
       zoom: this.zoom, // starting zoom
     });
     this.mapListeners();
   }
-
+  ngOnDestroy(): void {
+    this.map?.remove();
+  }
   mapListeners() {
     if (!this.map) throw new Error('No map reference');
     this.map.on('zoom', (ev) => {
@@ -33,6 +42,10 @@ export class ZoomRangePageComponent implements AfterViewInit {
     this.map.on('zoomend', (ev) => {
       if (this.map!.getZoom() < 18) return;
       this.map!.zoomTo(18);
+    });
+
+    this.map.on('move', (ev) => {
+      this.currentlngLat = this.map!.getCenter();
     });
   }
   zoomIn() {
